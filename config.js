297cff31e8fa93022ed111f2187c3b474c40a1c2
xVHY <cuzlocked@gmail.com>
2020-03-28 12:07:47 +0800

    Create basic config file

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	web: {
+		title: "SimpManager",
+		port: 2000
+	},
+	pgsql: {
+		user: "postgres",
+		host: "localhost",
+		database: "simpmanager",
+		password: "",
+		port: 5432
+	}
+}
