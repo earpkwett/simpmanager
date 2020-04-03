9bfdac8c6eff255b5ba3ff4cf8b12d3c455521ea
earpkwett <burcgd@protonmail.ch>
2020-04-03 13:53:37 +0800

    Add sessions and send title of page to client

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,29 @@
 const routes = require('express').Router();
 const api = require("./api");
 const bodyParser = require("body-parser");
+const session = require("express-session");
+const config = require("../config");
 
 routes.use(bodyParser.json());
 routes.use(bodyParser.urlencoded({extended: true}));
 
+routes.use(session({
+	secret: config.secrets.session_key,
+	resave: false,
+	saveUninitialized: true,
+	maxAge: config.session_length
+}));
+
 routes.get("/", (req, res) => {
-	res.render("index");
+	res.render("index", {title: config.web.title});
+})
+
+routes.get("/register", (req, res) => {
+	res.render("register", {title: config.web.title + " - Register"});
+})
+
+routes.get("/login", (req, res) => {
+	res.render("login", {title: config.web.title + " - Login"});
 })
 
 routes.use("/api", api);
