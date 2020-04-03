a0a9760f8ecfc86c2503b5304d4cf9f40c0ecfc7
earpkwett <burcgd@protonmail.ch>
2020-04-03 13:51:42 +0800

    Add password validation to config

--- config.js
+++ config.js
@@ -9,5 +9,13 @@ module.exports = {
 		database: "simpmanager",
 		password: "",
 		port: 5432
-	}
+	},
+	validation: {
+		password: {
+			min_length: 3,
+			max_length: 256
+		}
+	},
+	secrets: require("./secrets"),
+	session_length:  1000 * 60 * 60
 }
