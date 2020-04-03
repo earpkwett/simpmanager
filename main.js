48ced5c3fbd7bf0baa37955f12ea98c62c7b13ee
earpkwett <burcgd@protonmail.ch>
2020-04-03 13:52:19 +0800

    Allow client to retrieve content from res folder

--- main.js
+++ main.js
@@ -2,9 +2,23 @@ const express = require('express');
 const app = express();
 const routes = require("./routes");
 const config = require("./config");
+const fs = require('fs');
 
 app.set("view engine", "ejs");
 app.set("views", "./views")
+
+
+var views = require("path").join(__dirname, './views');
+app.get("/res/:name", (req, res) => {
+	let file = views + "/res/" + req.params.name;
+	fs.access(file, (err) => {
+		if (err) {
+			res.send("File not found!");
+		} else {
+			res.sendFile(file);
+		}
+	});
+})
 
 app.use("/", routes);
 
