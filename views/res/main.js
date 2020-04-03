0812797860bbcd87b43ac541344bde041888938c
earpkwett <burcgd@protonmail.ch>
2020-04-03 21:26:50 +0800

    Add some helper functions

--- views/res/main.js
+++ views/res/main.js
@@ -17,3 +17,19 @@ function API(method, url, data, cb) {
 		cb(res);
 	});
 }
+
+function sha512(str) {
+	return crypto.subtle.digest("SHA-256", new TextEncoder("utf-8").encode(str)).then(buf => {
+		return Array.prototype.map.call(new Uint8Array(buf), x=>(('00'+x.toString(16)).slice(-2))).join('');
+	});
+}
+
+function makeID(length) {
+	let result = "";
+	let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+	let charactersLength = characters.length;
+	for (let i = 0; i < length; i++) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
+	return result;
+}
